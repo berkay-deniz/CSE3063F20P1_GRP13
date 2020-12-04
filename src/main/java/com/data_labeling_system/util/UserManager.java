47ab81e5905a1c2216da0a56d58f0dd2866f38d8
ahmetfarukyilmaz <farukylm34@gmail.com>
2020-12-04 20:03:19 +0300

    Improvements on UserManager class

--- src/main/java/com/data_labeling_system/util/UserManager.java
+++ src/main/java/com/data_labeling_system/util/UserManager.java
@@ -1,21 +1,30 @@
 package com.data_labeling_system.util;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.json.*;
 import com.data_labeling_system.model.User;
 
 public class UserManager {
-	
+
 	private List<User> users;
-	
-	public  void createUsers(String json) {
+
+	public void createUsers(String json) {
 		JSONObject object = new JSONObject(json);
 		JSONArray userArray = object.getJSONArray("users");
-        users = new ArrayList<>();
-        for (int i = 0; i < userArray.length(); i++) {
-            users.add(new User(userArray.getJSONObject(i).toString()));
-        }
-		
+		this.users = new ArrayList<>();
+		for (int i = 0; i < userArray.length(); i++) {
+			this.users.add(new User(userArray.getJSONObject(i).toString()));
+		}
+
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public List<User> getUsers() {
+		return users;
 	}
 
 }
