7c95e1ee1a5b9e912ad3a30839bad597008908cb
rasitayaz <rasitayaz1358@gmail.com>
2020-12-05 01:40:12 +0300

    Restore App class

--- src/main/java/com/data_labeling_system/App.java
+++ src/main/java/com/data_labeling_system/App.java
@@ -1,20 +1,12 @@
 package com.data_labeling_system;
 
+import com.data_labeling_system.util.DataLabelingSystem;
+
 import java.io.IOException;
 
-import com.data_labeling_system.util.DataLabelingSystem;
-import org.apache.log4j.Logger;
-
 public class App {
-    static Logger logger=Logger.getLogger(App.class);
     public static void main(String[] args) throws IOException {
-    	//DataLabelingSystem dataLabelingSystem = new DataLabelingSystem();
-    	//dataLabelingSystem.startSystem();
-
-        logger.error("hata!");
-        logger.info("bilgi!");
-        logger.trace("trace!");
-        logger.warn("uyari!");
-        logger.fatal("fatal!");
+        DataLabelingSystem dataLabelingSystem = new DataLabelingSystem();
+        dataLabelingSystem.startSystem();
     }
 }
