c5b09bbf9c7c928ba8ca3368ea70b5a3b87a941c
ubeyd <ubeyd@192.168.1.29>
2020-12-04 18:57:23 +0300

    implements LabelingMechanism implements Assignment ,add Assignment() constructor

---
+++ src/main/java/com/data_labeling_system/mechanism/LabelingMechanism.java
@@ -0,0 +1,7 @@
+package com.data_labeling_system.mechanism;
+
+public abstract class LabelingMechanism {
+	public Assignment assign(User user,Instance instance,List<Label> labels,int maxNumOfLabels) {};
+	
+
+}
