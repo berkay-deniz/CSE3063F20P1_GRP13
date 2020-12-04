51afaaa31a56708427a212569fb439b7103aecd0
xbrkyx <brkydnz89@gmail.com>
2020-12-05 00:07:28 +0300

    add log4j

--- src/main/java/com/data_labeling_system/App.java
+++ src/main/java/com/data_labeling_system/App.java
@@ -3,10 +3,18 @@ package com.data_labeling_system;
 import java.io.IOException;
 
 import com.data_labeling_system.util.DataLabelingSystem;
+import org.apache.log4j.Logger;
 
 public class App {
+    static Logger logger=Logger.getLogger(App.class);
     public static void main(String[] args) throws IOException {
-    	DataLabelingSystem dataLabelingSystem = new DataLabelingSystem();
-    	dataLabelingSystem.startSystem();
+    	//DataLabelingSystem dataLabelingSystem = new DataLabelingSystem();
+    	//dataLabelingSystem.startSystem();
+
+        logger.error("hata!");
+        logger.info("bilgi!");
+        logger.trace("trace!");
+        logger.warn("uyari!");
+        logger.fatal("fatal!");
     }
 }
