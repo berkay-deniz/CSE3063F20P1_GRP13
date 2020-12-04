5484bdb30ccad775f3fe56516635512aed129b2f
Yunus-Yildirim <ynsyldrm6@gmail.com>
2020-12-05 02:13:00 +0300

    Add log4j for InstanceTagger

--- src/main/java/com/data_labeling_system/util/InstanceTagger.java
+++ src/main/java/com/data_labeling_system/util/InstanceTagger.java
@@ -3,28 +3,37 @@ package com.data_labeling_system.util;
 import com.data_labeling_system.mechanism.LabelingMechanism;
 import com.data_labeling_system.model.Assignment;
 import com.data_labeling_system.model.Dataset;
+import com.data_labeling_system.model.Instance;
 import com.data_labeling_system.model.User;
+import org.apache.log4j.Logger;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class InstanceTagger {
+	private final Logger logger;
 
     private Dataset dataset;
     private List<User> users;
 
     public InstanceTagger() {
+    	logger = Logger.getLogger(Instance.class);
     }
 
 
     public void assignLabels() {
         ArrayList<Assignment> assignments = new ArrayList<>();
+        logger.info("The list of assigment was created succesfully.");
         for (int i = 0; i < dataset.getInstances().size(); i++) {
             for (User user : users) {
                 LabelingMechanism labelingMechanism = user.getMechanism();
                 assignments.add(labelingMechanism.assign(user, dataset.getInstances().get(i),
                         dataset.getLabels(), dataset.getMaxNumOfLabels()));
+                logger.info("user id:"+user.getId()+" "+user.getName()+" tagged instance id:"+
+                        dataset.getInstances().get(i).getId()+" with class label:"+ dataset.getLabels()+
+                        "instance:"+dataset.getInstances().get(i));
             }
         }
         this.dataset.setAssignments(assignments);
