818ab1923f034014eb57b34d8b2ac7344bd71dd6
rasitayaz <rasitayaz1358@gmail.com>
2020-12-05 13:35:10 +0300

    Add no input file condition to IOManager

--- src/main/java/com/data_labeling_system/util/IOManager.java
+++ src/main/java/com/data_labeling_system/util/IOManager.java
@@ -1,10 +1,7 @@
 package com.data_labeling_system.util;
 
-
 import com.data_labeling_system.model.Dataset;
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.apache.log4j.Logger;
@@ -12,6 +9,7 @@ import org.apache.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
 public class IOManager {
@@ -22,7 +20,14 @@ public class IOManager {
     }
 
     public String readInputFile(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(fileName)));
+        String json = null;
+        try {
+            json = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (NoSuchFileException e) {
+            logger.error("Input file '" + fileName + "' not found.", e);
+            System.exit(1);
+        }
+        return json;
     }
 
     public void printFinalDataset(Dataset dataset, String outputFileName) throws IOException {
