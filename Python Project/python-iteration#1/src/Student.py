f580efea8148ab257bdc99a5634bcae462b0313a
ahmetemirhanbakkal <61470919+ahmetemirhanbakkal@users.noreply.github.com>
2021-01-16 01:03:47 +0300

    Arrangement of Student class.

--- Python Project/python-iteration#1/src/Student.py
+++ Python Project/python-iteration#1/src/Student.py
@@ -12,20 +12,32 @@ class Student:
         self.email = email
 
     def match(self, student_name):
-        if similar(self.name, student_name) > 0.70:
+        student_fullname = student_name.split()
+        number_array = '0123456789'
+        if student_fullname[0][0] in number_array:
+            student_fullname = student_fullname[1:]
+            student_name = ''
+            for word in student_fullname:
+                student_name += word
+                student_name += ' '
+        if similar(self.name.lower(), student_name.lower()) > 0.70:
             return True
-        elif similar(self.name, student_name) > 0.55:
+        elif similar(self.name.lower(), student_name.lower()) > 0.55:
             full_name = self.name.split()
             last_name = full_name[-1]
             student_full_name = student_name.split()
             student_last_name = student_full_name[-1]
-            if student_last_name == last_name:
+            if student_last_name.lower() == last_name.lower():
                 for name_element in full_name:
-                    if similar(name_element == student_full_name[0]) > 0.75:
+                    if similar(name_element.lower(),student_full_name[0].lower()) > 0.75:
                         return True
-                return False
+            return False
         else:
             return False
+
+
+
+
 
 
 
